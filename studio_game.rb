40af128e68e65c7e419506ae2580e03a66870217
RizwanSiddiqiGIT <74478286+RizwanSiddiqiGIT@users.noreply.github.com>
2021-01-19 17:52:19 -0500

    Numbers & Strings (Part 2)
    Print a List of Players

--- studio_game.rb
+++ studio_game.rb
@@ -1,4 +1,10 @@
-Player1Name = "larry"
-PLayer1Health = 60
+# Player1Name = "larry"
+# PLayer1Health = 60
 
-puts "#{Player1Name} has a health of #{PLayer1Health/9.0}"
+# puts "#{Player1Name} has a health of #{PLayer1Health/9.0}"
+
+
+Player1= "larry"
+Player2= "curly"
+Player3= "moe"
+puts "Players: \n\t#{Player1}\n\t#{Player2}\n\t#{Player3}"
