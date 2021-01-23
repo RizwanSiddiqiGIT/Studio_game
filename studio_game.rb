8add346a844b4a0570b37a149aee5d998b9e2943
RizwanSiddiqiGIT <74478286+RizwanSiddiqiGIT@users.noreply.github.com>
2021-01-22 19:31:17 -0500

    Variables&Objects

--- studio_game.rb
+++ studio_game.rb
@@ -5,6 +5,16 @@
 
 
 Player1= "larry"
+health1=60
 Player2= "curly"
+health2=125
 Player3= "moe"
+health3=100
+Player4="shemp"
+health4=90
+
+puts "#{Player1.capitalize} has a health of #{health1}"
+puts "#{Player3.capitalize} has a health of #{health3}.".center(50, '*')
+puts "#{Player4.capitalize.ljust(30,'.')} #{health4}"
+
 puts "Players: \n\t#{Player1}\n\t#{Player2}\n\t#{Player3}"
