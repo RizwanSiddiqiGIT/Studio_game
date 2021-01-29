0dfd20a2df4cea1f869d15086d4b5e336726db11
RizwanSiddiqiGIT <74478286+RizwanSiddiqiGIT@users.noreply.github.com>
2021-01-28 19:26:46 -0500

    Lesson 8.1
    Attributes

--- studio_game.rb
+++ studio_game.rb
@@ -1,11 +1,21 @@
+
+
 class Player
+    attr_accessor :name
+    attr_reader :health
+    def score
+        @health + @name.length
+    end
+
     def initialize(name, health=100)
         @name = name.capitalize
         @health = health
     end
+
+
     def to_s
-        "I'm #{@name} with a health of #{@health}."
-    end
+        "I'm #{@name} with a health of #{@health} and a score of #{score}."
+      end
 
     def blam
         @health -= 10
@@ -32,3 +42,10 @@ puts player3
 
 player3.woot 
 puts player3
+
+puts player1.health
+puts player2.health
+puts player3.health
+
+player2.name = "Lawrence"
+puts player2.name
