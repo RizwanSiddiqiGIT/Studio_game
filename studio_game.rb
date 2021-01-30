5071196461287edc5318941f30ddb4c07a419ca5
RizwanSiddiqiGIT <74478286+RizwanSiddiqiGIT@users.noreply.github.com>
2021-01-30 17:00:05 -0500

    Lesson 10.1
    Objects Interacting

--- studio_game.rb
+++ studio_game.rb
@@ -1,5 +1,3 @@
-
-
 class Player
     attr_accessor :name
     attr_reader :health
@@ -22,35 +20,42 @@ class Player
         puts "#{@name} got blammed!"
     end
 
-    def woot
+    def w00t
         @health += 15
         puts "#{@name} got wooted!"
     end
+end             # end of Player clas
+
+class Game
+    attr_reader :title
+    def initialize(title)
+        @title = title
+        @players = []
+    end
+    def add_player(a_player)
+        @players << a_player
+    end
+    def play
+        puts "There are #{@players.size} players in #{@title}: "
+        @players.each do |player|
+          puts player
+        end
+        @players.each do |player|
+          player.blam
+          player.w00t
+          player.w00t
+          puts player
+        end
+      end
 end
+
 
 player1 = Player.new("moe")
-puts player1
-
 player2 = Player.new("larry", 60)
-puts player2
 player3 = Player.new("curly", 125)
-puts player3
-players = [player1, player2, player3]
-players.pop
-player4 = Player.new("Shemp", 90)
-players.push(player4)
-players.each do |player|
-    puts player
-  end
-
-players.each do |player|
-    puts player.health
-end
-
-players.each do |player|
-    player.blam
-    player.woot
-    player.woot
-    puts player
-  end
-
+    
+knuckleheads = Game.new("Knuckleheads")
+knuckleheads.add_player(player1)
+knuckleheads.add_player(player2)
+knuckleheads.add_player(player3)
+knuckleheads.play
